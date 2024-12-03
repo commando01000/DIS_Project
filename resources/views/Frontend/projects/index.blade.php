1f60cf132f3523164e2705fd478880173dfc7a1a
Andrew Melad Makram <92456025+Andrew-120@users.noreply.github.com>
2024-12-03 22:23:42 +0200

    Projects

--- resources/views/Frontend/projects/index.blade.php
+++ resources/views/Frontend/projects/index.blade.php
@@ -1,4 +1,86 @@
 <div id="projects" class ="gh adjusted-scrolling w-75 mx-auto">
-    <h2 class="fa fa-bars">PROJECTS</h2>
-    <h1>WHAT WE'HE DONE</h1>
+    <div class="container">
+        <h2><span>O</span>ur <span>P</span>roducts</h2>
+        <p>Learn more about them. We have worked truly hard to make them perfect for every use.</p>
+        <div class="cards">
+          <div class="card" style="width: 18rem;">
+            <img src="images/1.jpg" class="card-img-top" alt="watch">
+            <div class="card-body">
+              <h5 class="card-title">Watch</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+            </div>
+          </div>
+
+          <div class="card" style="width: 18rem;">
+            <img src="images/2.jpg" class="card-img-top" alt="hand-watch">
+            <div class="card-body">
+              <h5 class="card-title">Hand-Watch</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+            </div>
+          </div>
+
+          <div class="card" style="width: 18rem;">
+            <img src="images/3.jpg" class="card-img-top" alt="shoes">
+            <div class="card-body">
+              <h5 class="card-title">Shoes</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+            </div>
+          </div>
+
+          <div class="card" style="width: 18rem;">
+            <img src="images/1.jpg" class="card-img-top" alt="watch">
+            <div class="card-body">
+              <h5 class="card-title">Watch</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+            </div>
+          </div>
+
+
+          <div class="remaining-products" style="display: none;">
+            <div class="card" style="width: 18rem;">
+              <img src="images/1.jpg" class="card-img-top" alt="watch">
+              <div class="card-body">
+                <h5 class="card-title">Watch</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                  content.</p>
+              </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+              <img src="images/2.jpg" class="card-img-top" alt="hand-watch">
+              <div class="card-body">
+                <h5 class="card-title">Hand-Watch</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                  content.</p>
+              </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+              <img src="images/3.jpg" class="card-img-top" alt="shoes">
+              <div class="card-body">
+                <h5 class="card-title">Shoes</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                  content.</p>
+              </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+              <img src="images/2.jpg" class="card-img-top" alt="hand-watch">
+              <div class="card-body">
+                <h5 class="card-title">Hand-Watch</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                  content.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="text-center mt-4">
+          <button id="readMoreBtn" class="btn btn-primary">Read More</button>
+        </div>
+      </div>
 </div>
