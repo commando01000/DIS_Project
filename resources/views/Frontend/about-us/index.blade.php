6fbab0be65dea7e41790b130af13f0d168a3f5b4
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2024-12-04 17:39:37 +0200

    more

--- resources/views/Frontend/about-us/index.blade.php
+++ resources/views/Frontend/about-us/index.blade.php
@@ -1,7 +1,11 @@
     <div id="about-us" class ="gh adjusted-scrolling w-75 mx-auto">
-        <h2 class="fa fa-book">{{ $translations[app()->getLocale()]['section_title'] ?? 'About Us' }}</h2>
-        <h1>{{ $translations[app()->getLocale()]['title'] ?? 'Who We Are' }}</h1>
+        <h2 class="fa fa-book"> {{-- {{ $translations[app()->getLocale()]['section_title'] ?? 'About Us' }} --}}ABOUT US </h2>
+        <h1>{{-- {{ $translations[app()->getLocale()]['title'] ?? 'Who We Are' }} --}} WHO WE ARE</h1>
         <p class="ppp">
-            {{ $translations[app()->getLocale()]['description'] ?? 'Your description here' }}
+            {{--  {{ $translations[app()->getLocale()]['description'] ?? 'Your description here' }}  --}}
+            A website (also written as a web site) is one or more web pages and related content that is identified by a
+            common domain name and published on at least one web server. Websites are typically dedicated to a
+            particular topic or purpose, such as news, education, commerce, entertainment, or social media.
+
         </p>
     </div>
