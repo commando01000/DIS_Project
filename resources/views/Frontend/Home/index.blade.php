a5ea6afd1c846be97b6ed726ad57a3f138499f6c
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2024-12-06 00:16:20 +0200

    projects edite

--- resources/views/Frontend/Home/index.blade.php
+++ resources/views/Frontend/Home/index.blade.php
@@ -188,6 +188,6 @@
 
 @section('js')
     <script>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
     </script>
 @endsection
--- resources/views/Frontend/projects/index.blade.php
+++ resources/views/Frontend/projects/index.blade.php
@@ -2,7 +2,7 @@
     <h2 class="fa fa-bars">PROJECTS</h2>
     <h1>OUR PROJECTS</h1>
     <div class="cards justify-content-center d-flex flex-wrap gap-5 mt-5">
-        <div class="card">
+        <div class="card" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModalLong">
             <img src="{{ asset('assets/images/1.jpg') }}" class="card-img-top" alt="watch">
             <div class="card-body">
                 <h5 class="card-title">Watch</h5>
@@ -11,7 +11,7 @@
             </div>
         </div>
 
-        <div class="card">
+        <div class="card" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModalLong">
             <img src="{{ asset('assets/images/2.jpg') }}" class="card-img-top" alt="hand-watch">
             <div class="card-body">
                 <h5 class="card-title">Hand-Watch</h5>
@@ -20,7 +20,7 @@
             </div>
         </div>
 
-        <div class="card">
+        <div class="card" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModalLong">
             <img src="{{ asset('assets/images/3.jpg') }}" class="card-img-top" alt="shoes">
             <div class="card-body">
                 <h5 class="card-title">Shoes</h5>
@@ -29,7 +29,7 @@
             </div>
         </div>
 
-        <div class="card">
+        <div class="card" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModalLong">
             <img src="{{ asset('assets/images/2.jpg') }}" class="card-img-top" alt="hand-watch">
             <div class="card-body">
                 <h5 class="card-title">Hand-Watch</h5>
@@ -37,5 +37,30 @@
                     card's content.</p>
             </div>
         </div>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog"
+            aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">Image Modal</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body text-center">
+                        <img src={{ asset('assets/images/3.jpg') }} alt="Image description" class="img-fluid mb-3">
+                        <p>This is the description of the image shown above.</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
     </div>
 </div>
