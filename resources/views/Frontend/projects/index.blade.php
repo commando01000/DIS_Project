3bff72f699dc9e6c412f23e846e58814fde79f9f
Andrew Melad Makram <92456025+Andrew-120@users.noreply.github.com>
2024-12-05 18:28:44 +0200

    Responsive large screens , Laptops, Tablets and mobile Phones for Section PROJECTS

--- resources/views/Frontend/projects/index.blade.php
+++ resources/views/Frontend/projects/index.blade.php
@@ -1,37 +1,41 @@
-<div id="projects" class ="gh adjusted-scrolling w-75 mx-auto">
+<div id="projects" class="gh adjusted-scrolling w-75 mx-auto">
     <h2 class="fa fa-bars">PROJECTS</h2>
     <h1>OUR PROJECTS</h1>
-    <div class="cards justify-content-center">
-        <div class="card" style="width: 18rem;">
+    <div class="cards justify-content-center d-flex flex-wrap gap-5 mt-5">
+        <div class="card">
             <img src="{{ asset('assets/images/1.jpg') }}" class="card-img-top" alt="watch">
             <div class="card-body">
                 <h5 class="card-title">Watch</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's
-                    content.</p>
+                    card's content.</p>
             </div>
         </div>
 
-        <div class="card" style="width: 18rem;">
+        <div class="card">
             <img src="{{ asset('assets/images/2.jpg') }}" class="card-img-top" alt="hand-watch">
             <div class="card-body">
                 <h5 class="card-title">Hand-Watch</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's
-                    content.</p>
+                    card's content.</p>
             </div>
         </div>
 
-        <div class="card" style="width: 18rem;">
+        <div class="card">
             <img src="{{ asset('assets/images/3.jpg') }}" class="card-img-top" alt="shoes">
             <div class="card-body">
                 <h5 class="card-title">Shoes</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's
-                    content.</p>
+                    card's content.</p>
             </div>
         </div>
 
+        <div class="card">
+            <img src="{{ asset('assets/images/2.jpg') }}" class="card-img-top" alt="hand-watch">
+            <div class="card-body">
+                <h5 class="card-title">Hand-Watch</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+            </div>
         </div>
     </div>
 </div>
