b873c82aa5eed044f09201706386ba778c2b656a
Andrew Melad Makram <92456025+Andrew-120@users.noreply.github.com>
2024-12-06 01:35:31 +0200

    OUR TEAM

--- resources/views/Frontend/team/index.blade.php
+++ resources/views/Frontend/team/index.blade.php
@@ -1,5 +1,18 @@
 <div id="our-team" class ="gh adjusted-scrolling w-75 mx-auto">
     <h2 class="fa fa-users">TEAM</h2>
-    <h1>OUR HARDWOR TEAM</h1>
+    <h1>OUR HARDWORK TEAM</h1>
+    <div class="cards justify-content-center d-flex flex-wrap gap-5 mt-5">
+        <div class="card">
+            <img src="{{ asset('assets/images/hero.jpg') }}" alt="Ahmed CEO">
+            <h3>Ahmed Abdelhay</h3>
+            <p class="sp">CEO</p>
+        </div>
+
+        <div class="card">
+            <img src="{{ asset('assets/images/1.jpg') }}" alt="Ahmed operation">
+            <h3>Ahmed Abdelhalim</h3>
+            <p class="sp">Operation Manager</p>
+        </div>
+    </div>
 
 </div>
