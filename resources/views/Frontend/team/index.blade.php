8a0c20754efb3484c00e1de695baa351c8182dec
Andrew Melad Makram <92456025+Andrew-120@users.noreply.github.com>
2024-12-06 22:22:40 +0200

    team updated

--- resources/views/Frontend/team/index.blade.php
+++ resources/views/Frontend/team/index.blade.php
@@ -1,4 +1,4 @@
-<div id="our-team" class ="gh adjusted-scrolling w-75 mx-auto">
+<div id="our-team" class="gh adjusted-scrolling w-75 mx-auto">
     <h2 class="fa fa-users">TEAM</h2>
     <h1>OUR HARDWORK TEAM</h1>
     <div class="cards justify-content-center d-flex flex-wrap gap-5 mt-5">
@@ -6,13 +6,20 @@
             <img src="{{ asset('assets/images/hero.jpg') }}" alt="Ahmed CEO">
             <h3>Ahmed Abdelhay</h3>
             <p class="sp">CEO</p>
+            <p class="contact">
+                <i class="fa fa-phone"></i> +201155879887<br>
+                <i class="fa fa-envelope"></i> ahmed.abdelhay@example.com
+            </p>
         </div>
 
         <div class="card">
             <img src="{{ asset('assets/images/5.png') }}" alt="Ahmed operation">
             <h3>Ahmed Abdelhalim</h3>
             <p class="sp">Operation Manager</p>
+            <p class="contact">
+                <i class="fa fa-phone"></i> +201045879872<br>
+                <i class="fa fa-envelope"></i> ahmed.abdelhalim@example.com
+            </p>
         </div>
     </div>
-
 </div>
