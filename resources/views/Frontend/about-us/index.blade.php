fb164eb7cffc5c1ece5f041b41ffa75aa4e58dc4
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-12-06 17:05:59 +0200

    Update index.blade.php

--- resources/views/Frontend/about-us/index.blade.php
+++ resources/views/Frontend/about-us/index.blade.php
@@ -1,5 +1,5 @@
 <div id="about-us"
-    class="gh adjusted-scrolling w-75 mx-auto {{ Settings::getValue('about-us')['status'] === 'on' ? '' : 'd-none' }}">
+    class="gh adjusted-scrolling w-75 mx-auto {{ Settings::getSettingValue('about-us')['status'] === 'on' ? '' : 'd-none' }}">
     <h2 class="fa fa-book">{{ translate('about-us')['section_title'] ?? 'About Us' }}</h2>
     <h1>{{ translate('about-us')['title'] ?? 'Who We Are' }}</h1>
     <p class="ppp">
