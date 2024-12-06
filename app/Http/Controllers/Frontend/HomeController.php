f4979e93b86bf7275503f60f505741eda2e0ccca
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-12-06 14:38:23 +0200

    Show and hide sections completed !

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\settings;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
--- resources/views/Frontend/about-us/index.blade.php
+++ resources/views/Frontend/about-us/index.blade.php
@@ -1,7 +1,8 @@
-    <div id="about-us" class ="gh adjusted-scrolling w-75 mx-auto">
-        <h2 class="fa fa-book">{{ translate('about-us')['section_title'] ?? 'About Us' }}</h2>
-        <h1>{{ translate('about-us')['title'] ?? 'Who We Are' }}</h1>
-        <p class="ppp">
-            {{ translate('about-us')['description'] ?? 'Your description here' }}
-        </p>
-    </div>
+<div id="about-us"
+    class="gh adjusted-scrolling w-75 mx-auto {{ Settings::getValue('about-us')['status'] === 'on' ? '' : 'd-none' }}">
+    <h2 class="fa fa-book">{{ translate('about-us')['section_title'] ?? 'About Us' }}</h2>
+    <h1>{{ translate('about-us')['title'] ?? 'Who We Are' }}</h1>
+    <p class="ppp">
+        {{ translate('about-us')['description'] ?? 'Your description here' }}
+    </p>
+</div>
