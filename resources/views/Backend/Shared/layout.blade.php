88114ab8b3557d20cba357482e44fa7fd49a0e2f
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-12-10 19:03:20 +0200

    Update layout.blade.php

--- resources/views/Backend/Shared/layout.blade.php
+++ resources/views/Backend/Shared/layout.blade.php
@@ -214,10 +214,10 @@
 
     <main class="container-fluid">
         <div class="row">
-            <div class="col-md-2">
+            <div class="col-md-3">
                 @include('Backend.Shared.sidebar')
             </div>
-            <div class="col-md-9">
+            <div class="col-md-8">
                 @yield('content')
             </div>
         </div>
