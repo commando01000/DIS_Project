4c180cfd8eb38534ce9ceff336d04870c997be31
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-12-12 21:08:47 +0200

    changes

--- resources/views/Backend/Shared/form-actions.blade.php
+++ resources/views/Backend/Shared/form-actions.blade.php
@@ -70,7 +70,7 @@
 </style>
 <div class="form-actions d-flex justify-content-between align-items-center">
     {{-- if route is not clients --}}
-    <input class="btn btn-success {{ Route::is('admin.client') ? 'd-none' : '' }}" name="translation" value="Save Translation"
+    <input class="btn btn-success" name="translation" value="Save Translation"
         type="submit" />
     <div class="toggle-container">
         <div class="toggle-switch">
