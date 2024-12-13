79b7ea7e0c5c347aa103ee9734c2902be5ab6182
Andrew Melad Makram <92456025+Andrew-120@users.noreply.github.com>
2024-12-13 03:31:47 +0200

    team updated

--- resources/views/Frontend/team/index.blade.php
+++ resources/views/Frontend/team/index.blade.php
@@ -2,13 +2,13 @@
     <h2 class="fa fa-users">TEAM</h2>
     <h1>OUR HARDWORK TEAM</h1>
     <div class="cards justify-content-center d-flex flex-wrap gap-5 mt-5">
-        <div class="card">
+        <div class="card position-relative">
             <img src="{{ asset('assets/images/hero.jpg') }}" alt="Ahmed CEO">
             <h3>Ahmed Abdelhay</h3>
             <div class="qr-code" data-url="https://example.com/ahmed-ceo"></div>
         </div>
 
-        <div class="card">
+        <div class="card position-relative">
             <img src="{{ asset('assets/images/5.png') }}" alt="Ahmed operation">
             <h3>Ahmed Operation</h3>
             <div class="qr-code" data-url="https://example.com/ahmed-ceo"></div>
