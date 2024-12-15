f77b39f8987bf4be1f2c4e9eabed06246d30bd5f
Andrew Melad Makram <92456025+Andrew-120@users.noreply.github.com>
2024-12-15 14:24:32 +0200

    Team frontend combine with back

--- resources/views/Frontend/team/index.blade.php
+++ resources/views/Frontend/team/index.blade.php
@@ -2,17 +2,13 @@
     <h2 class="fa fa-users">TEAM</h2>
     <h1>OUR HARDWORK TEAM</h1>
     <div class="cards justify-content-center d-flex flex-wrap gap-5 mt-5">
-        <div class="card position-relative">
-            <img src="{{ asset('assets/images/testimonials/1.jpg') }}" alt="Ahmed CEO">
-            <h3>Ahmed Abdelhay</h3>
-            <div class="qr-code" data-url="https://example.com/ahmed-ceo"></div>
-        </div>
-
-        <div class="card position-relative">
-            <img src="{{ asset('assets/images/testimonials/2.jpg') }}" alt="Ahmed operation">
-            <h3>Ahmed Operation</h3>
-            <div class="qr-code" data-url="https://example.com/ahmed-ceo"></div>
-        </div>
+        @foreach ($teammembers->take(2) as $member)
+            <div class="card position-relative">
+                <img src="{{ asset($member-> image) }}" alt="{{$member-> name[app()->getLocale()] ?? 'name image here'}}">
+                <h3>{{$member-> name[app()->getLocale()] ?? 'name here'}}</h3>
+                <div class="qr-code" data-url="{{$member-> profileURL[app()->getLocale()] ?? 'Profile URL here'}}"></div>
+            </div>
+        @endforeach
     </div>
     <div class="gradient-line"></div>
 </div>
