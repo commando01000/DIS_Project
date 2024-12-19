94276568c887c8947375de4452decc4b8d753408
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-12-19 23:01:11 +0200

    Fixed Home !

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -44,7 +44,6 @@ class HomeController extends Controller
             $testimonials = cache()->remember('testimonials', now()->addMinutes(30), function () {
                 return Testimonial::paginate(9);
             });
-
             return view('Frontend.home.Index', compact('clients', 'projects', 'settings', 'testimonials'));
         } catch (\Exception $e) {
             // Handle the exception (e.g., log it, show an error message, etc.)
@@ -70,8 +69,8 @@ class HomeController extends Controller
             'name' => $testimonial->name[$locale] ?? 'N/A',
             'role' => $testimonial->role[$locale] ?? 'N/A',
             'description' => $testimonial->description[$locale] ?? 'N/A',
-            'phone'=>$testimonial->phone ?? 'N/A',
-            'mail'=>$testimonial->mail ?? 'N/A',
+            'phone' => $testimonial->phone ?? 'N/A',
+            'mail' => $testimonial->mail ?? 'N/A',
             'image' => $testimonial->image ?? 'default-image.png',
             'social_media' => $testimonial->social_media ? json_decode($testimonial->social_media, true) : [],
         ];
